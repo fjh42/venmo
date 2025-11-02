3f11c215ee0190475ae5efe3a3093e52154ce4a1
fjh42 <fjh42@cornell.edu>
2025-11-02 01:46:05 -0400

    Implemented database and API methods

--- src/app.py
+++ src/app.py
@@ -11,9 +11,78 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
+@app.route("/api/users/",methods=["GET"])
+def get_all_users():
+    """
+    Get all users from the database.
+    """
+    return json.dumps({"users":DB.get_all_users()}),200
 
-# your routes here
+@app.route("api/users/<int:user_id>/",methods=["GET"])
+def get_user_by_id(user_id):
+    """
+    Get an specific user by their id.
+    """
+    user = DB.get_specific_user(user_id)
+    if user is None:
+        return json.dumps({"error":"User not found."}),404
+    
+    return json.dumps(user),200
+
+@app.route("api/users/",methods=["POST"])
+def create_new_user():
+    """
+    Create a new user with 'name','username', and 'balance'. Default balance to 0 if not provided.
+    """
+    body = json.loads(request.data)
+    name = body.get("name")
+    username = body.get("username")
+
+    if name is None or username is None:
+        return json.dumps({"error":"Name or Username not provided."}),400
+
+    balance = body.get("balance",0)
+
+    new_user_id = DB.create_new_user(name,username,balance)
+    new_user = DB.get_specific_user(new_user_id)
+
+    return json.dumps(new_user),201
+
+@app.route("api/users/<int:user_id>/",methods=["DELETE"])
+def delete_user(user_id):
+    user = DB.get_specific_user(user_id)
+    if not user:
+        return json.dumps({"error":"User not found."}),404
+
+    DB.delete_user(user_id)
+    return json.dumps(user),200
+
+@app.route("/api/send/",methods=["POST"])
+def send_money():
+    """
+    Send money from one user to another.
+    """
+    body = json.loads(request.data)
+    sender_id = body.get("sender_id")
+    receiver_id = body.get("receiver_id")
+    amount = body.get("amount")
+
+    if sender_id is None or receiver_id is None or not isinstance(amount,int):
+        return json.dumps({"error":"Transaction invalid. Must provide a sender_id, a receiver_id, and an integer amount."}),400
+    
+    sender = DB.get_specific_user(sender_id)
+    receiver = DB.get_specific_user(receiver_id)
+
+    if not sender or not receiver:
+        return json.dumps({"error":"Sender or Receiver  not found."}),404
+    
+    new_sender_balance = sender.get("balance") - amount
+    new_receiver_balance = receiver.get("balance") + amount
+    DB.update_users_balance(sender_id,new_sender_balance,receiver_id,new_receiver_balance)
+
+    return json.dumps({"sender_id":sender_id, "receiver_id":receiver_id,"amount": amount}),200
 
 
 if __name__ == "__main__":
+    DB.create_users_table()
     app.run(host="0.0.0.0", port=5000, debug=True)
--- src/db.py
+++ src/db.py
@@ -17,11 +17,91 @@ class DatabaseDriver(object):
     """
     Database driver for the Task app.
     Handles with reading and writing data with the database.
+    
     """
 
     def __init__(self):
-        pass
+        """
+        Secures a connection with the database and stores it into the instance variable 'conn'
+        """
+        self.conn = sqlite3.connect(
+            "todo.db", check_same_thread= False
+        )
 
+    def create_users_table(self):
+        """
+        Using SQL, create users table with NULL balance.
+        """
+        self.conn.execute("""
+                          CREATE TABLE IF NOT EXISTS users(
+                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name TEXT NOT NULL,
+                          username TEXT NOT NULL,
+                          balance INTEGER DEFAULT 0
+                          );
+                          """)
+        
+    def delete_users_table(self):
+        """
+        Using SQL, delete the users table
+        """
+        self.conn.execute("DROP TABLE IF EXISTS users;")
+    
+    def get_all_users(self):
+        """
+        Using SQL, get all users in the table users.
+        """
+        cursor = self.conn.execute("SELECT * FROM users;")
+        users = []
+        for row in cursor:
+            users.append({"id": row[0],"name": row[1], "username": row[2], "balance": row[3]})
+
+        return users
+    
+    def get_specific_user(self,id):
+        """
+        Using SQL, get a user by its id.
+        """
+        cursor = self.conn.execute("SELECT * FROM users WHERE id = ?;",(id,))
+        for row in cursor:
+            return {"id": row[0],"name": row[1], "username": row[2], "balance" : row[3]}
+        return None
+
+    def create_new_user(self,name,username,balance=0):
+        """
+        Using SQL, add a new user to the table users.
+        """
+        cursor = self.conn.execute("INSERT INTO users (name,username,balance) VALUES (?,?,?);" , (name,username,balance))
+        self.conn.commit()
+        return cursor.lastrowid
+    
+    def update_users_balance(self,sender_id,sender_afterb,receiver_id,receiver_afterb):
+        """
+        Using SQL, update the table users after a transaction has been made.
+        Substract from the sender's balance and add to the reciever's.
+        """
+        self.conn.execute("""
+            UPDATE users
+            SET balance = ?
+            WHERE id = ?
+            """, (sender_afterb, sender_id))
+
+        self.conn.execute("""
+            UPDATE users
+            SET balance = ?
+            WHERE id = ?
+            """, (receiver_afterb, receiver_id))
+
+        self.conn.commit()
+
+    def delete_user(self,id):
+        """
+        Using SQL, delete a user from users.
+        """
+        self.conn.execute("""
+            DELETE FROM users
+            WHERE id = ?;
+        """, (id,))
 
 # Only <=1 instance of the database driver
 # exists within the app at all times
